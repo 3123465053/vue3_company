1361c6fa5f83453a11bf29e0c9aefb3447efc4c9
3123465053 <3123465053@qq.com>
2024-11-18 17:58:00 +0800

    11.18

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
   <h1>一级标题</h1>
+  <el-button type="primary" size="default" :icon="Edit"></el-button>
+  <h2>svg图标加载</h2>
+
+  <!-- 自己封装了一次 -->
+   <svg-icon name="phone" color="red" width="20px"></svg-icon>
+
+ <!-- 直接使用 -->
+    <!-- <svg>
+      <use xlink:href="#icon-phone" fill="red"></use>
+    </svg> -->
 </template>
+
+<script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue';
+ import SvgIcon from '@/components/SvgIcon/index.vue'
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from "element-plus"
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+//@ts-ignore   忽略类型检测，避免打包时错误
+import  zh_cn  from 'element-plus/dist/locale/zh-cn.mjs'
+
+
+
+//获取应用实例对象
+const app=createApp(App)
+
+//安装element-plus插件
+app.use(ElementPlus,{
+    locale:zh_cn      //element-plus 国际化配置
+})
+ 
+console.log(import.meta.env)
+
+//svg插件需要配置代码
+//@ts-ignore
+import 'virtual:svg-icons-register'
+
+//将应用挂载到挂载点上
+app.mount('#app')
